b7c2101d907bae9de03a4d6ce01a26b8ac8a51f1
aj <toalexjesus@gmail.com>
2026-03-16 16:23:23 +0200

    Add null check for images in detail page

--- src/routes/synths/[brand]/[series]/[model]/+page.svelte
+++ src/routes/synths/[brand]/[series]/[model]/+page.svelte
@@ -31,7 +31,7 @@
 
   <section class="hero-media">
     <div class="hero-image-wrap">
-      <img src={synth.images[0]} alt={synth.modelName} loading="lazy" decoding="async" />
+      <img src={synth.images && synth.images[0] ? synth.images[0] : ''} alt={synth.modelName} loading="lazy" decoding="async" />
       <span class="badge badge-price">{releasePrice ? `$${releasePrice}` : 'н/д'}</span>
       <span class="badge badge-year">{synth.year}</span>
     </div>
